630f7fc7aa84430448f183f7a1fbeb0fafb4443d
Jakub Lewalski <jakub.lewalski@gmail.com>
2016-05-31 00:30:21 +0200

    Pagination for Products page

--- app/models/Product.java
+++ app/models/Product.java
@@ -1,11 +1,14 @@
 package models;
 
 import java.util.*;
-
+import javax.persistence.*;
 import javax.validation.*;
+
+import play.data.format.*;
 import play.data.validation.Constraints.*;
 import play.db.ebean.*;
-import javax.persistence.*;
+
+import com.avaje.ebean.*;
 
 @Entity
 public class Product extends Model {
@@ -29,14 +32,21 @@ public class Product extends Model {
     @ManyToOne
     public ProductMeal productMeal;
     
-    public static Finder<Long,Product> find = new Finder(
-        Long.class, Product.class
-    );
+    public static Finder<Long,Product> find = new Finder(Long.class, Product.class);
     
     public static List<Product> all() {
         return find.all();
     }
-    
+
+    public static Page<Product> page(int page, int pageSize, String sortBy, String order, String filter) {
+	    return find.where()
+		    .ilike("name", "%" + filter + "%")
+		    .orderBy(sortBy + " " + order)
+		    .findPagingList(pageSize)
+		    .setFetchAhead(false)
+		    .getPage(page);
+    }
+
     public static void create(Product product) {
         product.save();
     }
